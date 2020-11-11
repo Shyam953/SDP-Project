073e46d31101f27148c7b8db58b801cb3c1acbc2
shyam bhadeshiya <63584917+Shyam953@users.noreply.github.com>
2020-11-11 14:35:36 +0530

    Update views.py

--- PDF2PPT-main/pdfppt/views.py
+++ PDF2PPT-main/pdfppt/views.py
@@ -52,6 +52,15 @@ def edit_lists(request):
 
         prs = Presentation()
         bullet_slide_layout = prs.slide_layouts[1] # 1 - Title and Content
+        # 0 - Title (presentation title slide)
+        # 1 - Title and Content
+        # 2 - Section Header (sometimes called Segue)
+        # 3 - Two Content (side by side bullet textboxes)
+        # 4 - Comparison (same but additional title for each side by side content box)
+        # 5 - Title Only
+        # 6 - Blank
+        # 7 - Content with Caption
+        # 8 - Picture with Caption
 
         for title in root:
             c = title.get('id')
